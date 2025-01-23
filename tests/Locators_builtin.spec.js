c90c15583054424f3aa11ea745d5ca4aed5dc7a2
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2025-01-22 20:34:01 -0500

    added assertions

--- tests/Locators_builtin.spec.js
+++ tests/Locators_builtin.spec.js
@@ -4,7 +4,8 @@ const { log } = require('console')
 
 test ('Built-in locators', async ({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    const logo=await page.getByAltText("company-branding")
+    const logo= await page.getByAltText("company-branding")
     await expect (logo).toBeVisible();
-    
+    //page.getByPlaceHolder ()-- to locate an input by placeholder
+    await page.getByPlac
 })
