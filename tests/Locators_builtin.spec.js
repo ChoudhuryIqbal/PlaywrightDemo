d05a92d547abeb43e447ad8f72c3e1dc8ff13fc0
Choudhury Iqbal <iqbalchoudhury2@gmail.com>
2025-01-25 20:27:04 -0500

    dropdown

--- tests/Locators_builtin.spec.js
+++ tests/Locators_builtin.spec.js
@@ -1,5 +1,4 @@
-const {test, expect} =require ('@playwright/test'
-)
+const {test, expect} =require ('@playwright/test')
 const { log } = require('console')
 
 test ('Built-in locators', async ({page})=>{
